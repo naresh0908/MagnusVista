5f76f15a7c718c54356d3075626c013c783ff99f
naresh0908 <nareshp2026@gmail.com>
2024-07-13 15:08:46 +0530

    src folder in npm

---
+++ Quiz_3(src)/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import QuizComponent from './QuizComponent';
+
+function App() {
+    return (
+        <div className="App">
+            <QuizComponent />
+        </div>
+    );
+}
+
+export default App;
